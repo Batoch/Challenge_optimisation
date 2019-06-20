fbf834b76da3bd542c783304ab52fcde801ce18e
Batoch <baptiste.massardier@gmail.com>
2019-06-20 16:21:35 +0200

    oui

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import csv
+import numpy as np
 
 position = []
 operation = []
-Baie = []
 Hauteur = []
 operations = []
-CT = [0, 0, 0]
+CT = [0]
+
 
 with open('1_global.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         taille = row
 
-#print(taille)
+
 N = int(taille[0])
 L = int(taille[1])
 H = int(taille[2])
+
+Baie = np.zeros((L, H))
 
 
 with open('1_position.csv', newline='') as csvfile:
@@ -25,7 +28,7 @@ with open('1_position.csv', newline='') as csvfile:
 position = position[1:]
 
 for i in range(len(position)):
-    position[i][0] = position[i][0][:len(position[i][0])-1]
+    position[i][0] = position[i][0][2:len(position[i][0])-1]
     position[i][1] = int(position[i][1][1:2])
     position[i][2] = int(position[i][2][1:2])
 
@@ -37,22 +40,8 @@ with open('1_operations.csv', newline='') as csvfile:
 
 operation = operation[1:]
 for i in range(len(operation)):
-    operation[i][0] = operation[i][0][:3]
+    operation[i][0] = operation[i][0][:len(operation[i][0])-1]
     operation[i][1] = operation[i][1][1:2]
-
-
-
-
-# for i in range(H):       #Hauteur
-#     Hauteur.append(0)
-#
-# for i in range(L):      #Largeur
-#     Baie.append(Hauteur)
-
-
-
-
-
 
 
 
@@ -64,81 +53,78 @@ def firstfit():
                 i += 1
             ajout(op[0], i)
         else:
-            col, haut = trouverconteneur(op[0])
-            while taillecolonne(Baie[col]) > haut:
-                deplacer(col, premierepilenonpleine())
+            col, haut = trouverconteneur(op[0][2:])
+            a = taillecolonne(col)-1
+            while taillecolonne(col)-1 > haut:
+                if premierepilenonpleine(0) == col:
+                    deplacer(col, premierepilenonpleine(col+1))
+                else:
+                    deplacer(col, premierepilenonpleine(0))
             retrait(col)
+            print(Baie)
 
 
 
 
 def colonneplein(colonne):          #return 0 si non pleine, 1 sinon
-    if taillecolonne(Baie[colonne]) >= 5:
+    if taillecolonne(colonne) >= 5:
         return 1
     return 0
 
 
 def retrait(colonne):
     global operations
-    Baie[colonne][taillecolonne(colonne)].remove()
-    operations += [colonne, 0]
+    Baie[colonne, taillecolonne(colonne)-1] = 0
+    operations.append([str(colonne+1) + " ", " " + str(0)])
 
 
 def ajout(cont, i):                 #ajoute le conteneur cont a la colonne i
     global operations
-    Baie[i].append(cont)
-    operations += [0, i]
+    Baie[i, taillecolonne(i)] = cont
+    operations.append([str(0) + " ", " " + str(i+1)])
 
 
 def deplacer(Colonneactuelle, Colonnedesire):
     global operations
-    ajout(Baie[Colonneactuelle][taillecolonne(Colonneactuelle)], Colonnedesire)
-    Baie[Colonneactuelle][taillecolonne(Colonneactuelle)].remove()
-    operations += [Colonneactuelle, Colonnedesire]
+    a = taillecolonne(Colonneactuelle)
+    ajout(Baie[Colonneactuelle, taillecolonne(Colonneactuelle)-1], Colonnedesire)
+    retrait(Colonneactuelle)
+    operations.append([str(Colonneactuelle+1) + " ", " " + str(Colonnedesire+1)])
 
 
 def taillecolonne(colonne):
     for i in range(H):
-        if i > H:
-            return H
-        if Baie[colonne][i] == [0, 0, 0]:
+        if Baie[colonne, i] == [0]:
             return i
+    return H
 
 
 def trouverconteneur(nom):
     for i in range(L):
         for j in range(H):
-            print(Baie[i][j])
-            if Baie[i][j] == nom:
+            if Baie[i, j] == int(nom):
                 return i, j
 
 
-def premierepilenonpleine():
-    i = 0
+def premierepilenonpleine(i):               #premiere pile non pleine a partir de la pile i
     while colonneplein(i):
         i += 1
     return i
 
 
 def ecrituresol():
-    with open('1_detailSolutiontest.csv', 'w', newline='') as csvfile:
+    with open('1_detailSolution.csv', 'w', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
-        spamwriter.writerow(['FROM'] + ['TO'])
+        spamwriter.writerow(['FROM '] + [' TO'])
         for i in range(len(operations)):
             spamwriter.writerow(operations[i])
 
 
 def init():
-    for j in range(H):
-        Hauteur.append(CT)
-    for i in range(L):
-        Baie.append(Hauteur)
-    print(Hauteur)
-    print(Baie)
+
     for i in range(len(position)):
-        print(position[i][1])
-        Baie[position[i][1]-1][position[i][2]-1] = [position[i][0]]
+        Baie[position[i][1]-1, position[i][2]-1] = position[i][0]
 
 
 init()
